5d434f093e038dfebc881d1b350ba0b5a5f77c5f
tazbirristy <tazbirristyweb@gmail.com>
2022-10-26 05:42:55 +0600

    server ready to fetch data

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("course API");
+});
+app.get("/course-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const courseCategory = courses.filter((course) => course.category_id === id);
+  res.send(courseCategory);
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const detailsCourses = courses.find((course) => course._id === id);
+  res.send(detailsCourses);
+});
 
 app.get("/", (req, res) => {
   res.send("Educine API running");
